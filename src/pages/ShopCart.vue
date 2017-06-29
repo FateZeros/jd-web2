49a5ca4246283af22a2bd210f071885015140c51
FateZeros <490293266@qq.com>
2017-06-29 23:06:31 +0800

    shopcart

--- src/pages/ShopCart.vue
+++ src/pages/ShopCart.vue
@@ -35,7 +35,54 @@
 					</div>
 				</div>
 				<div class="cart-wrap">
-					
+					<!-- 购物车 头部 -->
+					<table class="cart-thead">
+						<tr class="thead-row">
+							<td class="thead-check">
+								<div class="cart-checkbox">
+									<input type="checkbox">
+									<label for="">勾选全部商品</label>
+								</div>
+								<span>全选</span>
+							</td>
+							<td class="thead-goods">商品</td>
+							<td class="thead-price">单价</td>
+							<td class="thead-nums">数量</td>	
+							<td class="thead-sum">小计</td>
+							<td class="thead-action">操作</td>
+						</tr>
+					</table>
+					<!-- 购物车 主体 -->
+					<div class="cart-tbody">
+						<div class="shop">
+							<div class="cart-checkbox">
+								<input type="checkbox">
+								<label for="">勾选全部商品</label>
+							</div>
+							<a>{{shopName}}</a>
+							<div class="shop-extra">
+								{{shopExtra}}
+							</div>
+						</div>
+						<div class="item-list">
+							<div class="item-single">
+								<div class="cart-checkbox">
+									<input type="checkbox">
+									<label for="">勾选当前商品</label>
+								</div>
+								<div class="cart-good">
+									<div class="good-brief">
+										<img :src="cartImg" alt="" />
+										<div class="good-info">
+											韩都衣舍2017夏装新款韩版女装假两件网纱收腰连衣裙MR6262陆 白色 M
+										</div>
+									</div>
+									<div>1212</div>
+									<div>1212</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -47,10 +94,16 @@
 	import NavBar from '../components/NavBar.vue'
 	import AFooter from '../components/Footer.vue'
 
+	// 测试图片
+	import cart01 from '../assets/img/cart01.jpg'
+
 	export default {
 		data() {
 			return {
-				cartNum: 1
+				cartNum: 1,
+				shopName: '韩都衣舍旗舰店',
+				shopExtra: '已免运费',
+				cartImg: cart01
 			}
 		},
 		methods: {
@@ -201,6 +254,111 @@
 			 	font-size: 14px;
 			 	color: #666;
 			 	text-align: right;
+			}
+		}
+
+		.cart-wrap {
+			.cart-thead {
+				width: 100%;
+				height: 40px;
+				line-height: 40px;
+				background: #f3f3f3;
+				border: 1px solid #e9e9e9;
+				font-size: 12px;
+
+				.thead-row {
+					display: flex;
+					padding: 0 10px;
+
+					.thead-check {
+						width: 122px;
+					}
+
+					.thead-goods {
+						flex: 1;
+					}
+
+					.thead-price {
+						width: 120px;
+						text-align: right;
+					}
+
+					.thead-nums {
+						width: 80px;
+						text-align: center;
+					}
+
+					.thead-sum {
+						width: 100px;
+						text-align: right;
+					}
+
+					.thead-action {
+						width: 75px;
+						text-align: center;
+					}
+				}
+
+			}
+
+			.cart-checkbox {
+				float: left;
+				margin-right: 8px;
+
+				label {
+					display: none;
+				}
+			}
+
+			.cart-tbody {
+				margin-top: 20px;
+
+				.shop {
+					height: 30px;
+					line-height: 30px;
+					font-weight: 700;
+					padding: 0 10px;
+					font-size: 14px;
+
+					.shop-extra {
+						float: right;
+						font-weight: 400;
+						font-size: 12px;
+					}
+				}
+
+				.item-list {
+					border-top: 2px solid #aaa;
+					position: relative;
+					padding: 0 10px;
+
+					.item-single {
+						.cart-good {
+							height: 110px;
+							display: flex;
+							padding: 15px 0;
+							border: 1px solid;
+						}
+
+						.good-brief {
+							width: 300px;
+							height: 84px;
+							border: 1px solid;
+							display: flex;
+
+							img {
+								width: 80px;
+								height: 80px;
+							}
+
+							.good-info {
+								flex: 1;
+								height: 80px;
+								font-size: 12px;
+							}
+						}
+					}
+				}
 			}
 		}
 
